6b136120fc82a746dfa2ffc8fac5797102359cf6
Steven Smail <41452253+ssmail7@users.noreply.github.com>
2021-02-13 21:04:52 -0500

    Update Fraction.h

--- Fraction.h
+++ Fraction.h
@@ -1,4 +1,4 @@
-//***************************************************************
+//****************************************************************
 // Written by Steven Smail for COP3331
 //
 // Fraction.h (Project 3)
@@ -7,18 +7,27 @@
 // function prototypes for the Constructor, the Mutator function,
 // the Operator functions for + - * /, and the Operator functions
 // for << and >>.
-//***************************************************************
+//****************************************************************
 
 #ifndef FRACTION_H
 #define FRACTION_H
 
 #include <iostream>
-using namespace std;// Fraction class declarationclass Fraction{  private:    int numerator;    int denominator;
-    void simplify();  public:
+using namespace std;
+
+// Fraction class declaration
+class Fraction
+{
+  private:
+    int numerator;
+    int denominator;
+    void simplify();
+  public:
     // Constructor
     Fraction();
 
-    // Mutator    void setFraction(int a, int b);
+    // Mutator
+    void setFraction(int a, int b);
 
     // Overloaded + - * and /
     Fraction operator + (const Fraction &);
@@ -29,5 +38,7 @@ using namespace std;// Fraction class declarationclass Fraction{  private:
     // Overloaded << and >>
     friend ostream& operator << (ostream&, const Fraction&);
     friend istream& operator >> (istream&, Fraction&);
-};
-#endif
+
+};
+
+#endif
