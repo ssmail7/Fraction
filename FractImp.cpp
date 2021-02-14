1c48bd3e58a6e75d4f0b37e8214d45f57a6e35b9
Steven Smail <41452253+ssmail7@users.noreply.github.com>
2021-02-13 21:03:51 -0500

    Update FractImp.cpp

--- FractImp.cpp
+++ FractImp.cpp
@@ -1,4 +1,4 @@
-//***************************************************************
+//**************************************************************
 // Written by Steven Smail for COP3331
 //
 // FractImp.cpp (Project 3)
@@ -7,22 +7,26 @@
 // definitions for the Constructor, the Mutator function, the
 // Operator functions for + - * /, and the Operator functions
 // for << and >>.
-//***************************************************************
+//**************************************************************
 
 #include <cstdlib>     // Needed for abs()
 #include "Fraction.h"
 using namespace std;
 
 // Constructor
-Fraction::Fraction(){ numerator = 0;
+Fraction::Fraction()
+{ numerator = 0;
   denominator = 1;
   simplify(); }
 
-// Mutator functionvoid Fraction::setFraction(int a, int b){ numerator = a;
+// Mutator function
+void Fraction::setFraction(int a, int b)
+{ numerator = a;
   denominator = b;
   simplify(); }
 
-//**********************************************// Definition of member function simplify.
+//**********************************************
+// Definition of member function simplify.
 // This function simplifies each fraction into
 // it's most reduced form.
 //**********************************************
@@ -54,44 +58,97 @@ void Fraction::simplify()
   }
 }
 
-//**********************************************// Overloaded + operator
-//**********************************************Fraction Fraction::operator + (const Fraction &right){  Fraction temp;  if(denominator == right.denominator)
+//**********************************************
+// Overloaded + operator
+//**********************************************
+Fraction Fraction::operator + (const Fraction &right)
+{
+  Fraction temp;
+
+  if(denominator == right.denominator)
   {
     temp.numerator = numerator + right.numerator;
     temp.denominator = denominator;
-  }  else
-  {    temp.numerator = (numerator * right.denominator) +
+  }
+
+  else
+  {
+    temp.numerator = (numerator * right.denominator) +
                      (right.numerator * denominator);
     temp.denominator = denominator * right.denominator;
   }
-  temp.simplify();  return temp;}//**********************************************// Overloaded - operator
-//**********************************************Fraction Fraction::operator - (const Fraction &right){  Fraction temp;  if(denominator == right.denominator)
+  temp.simplify();
+  return temp;
+}
+
+//**********************************************
+// Overloaded - operator
+//**********************************************
+Fraction Fraction::operator - (const Fraction &right)
+{
+  Fraction temp;
+
+  if(denominator == right.denominator)
   {
     temp.numerator = numerator - right.numerator;
     temp.denominator = denominator;
-  }  else
-  {    temp.numerator = (numerator * right.denominator) -
+  }
+
+  else
+  {
+    temp.numerator = (numerator * right.denominator) -
                      (right.numerator * denominator);
     temp.denominator = denominator * right.denominator;
   }
-  temp.simplify();  return temp;}
+  temp.simplify();
+  return temp;
+}
 
-//**********************************************// Overloaded * operator
-//**********************************************Fraction Fraction::operator * (const Fraction &right){  Fraction temp;  temp.numerator = numerator * right.numerator;  temp.denominator = denominator * right.denominator;
-  temp.simplify();  return temp;}
+//**********************************************
+// Overloaded * operator
+//**********************************************
+Fraction Fraction::operator * (const Fraction &right)
+{
+  Fraction temp;
 
-//**********************************************// Overloaded / operator
-//**********************************************Fraction Fraction::operator / (const Fraction &right){  Fraction temp;  temp.numerator = numerator * right.denominator;  temp.denominator = denominator * right.numerator;
-  temp.simplify();  return temp;}
+  temp.numerator = numerator * right.numerator;
+  temp.denominator = denominator * right.denominator;
+  temp.simplify();
+  return temp;
+}
+
+//**********************************************
+// Overloaded / operator
+//**********************************************
+Fraction Fraction::operator / (const Fraction &right)
+{
+  Fraction temp;
+
+  temp.numerator = numerator * right.denominator;
+  temp.denominator = denominator * right.numerator;
+  temp.simplify();
+  return temp;
+}
 
 //**********************************************
 // Overloaded << operator
-//**********************************************ostream& operator << (ostream& os, const Fraction& fraction)
-{  // Note that we print out a / as it is simply easier to do so!  os << fraction.numerator << " / " << fraction.denominator;  return os;
+//**********************************************
+ostream& operator << (ostream& os, const Fraction& fraction)
+{
+  // Note that we print out a / as it is simply easier to do so!
+  os << fraction.numerator << " / " << fraction.denominator;
+  return os;
 }
 
-//**********************************************// Overloaded >> operator
-//**********************************************istream& operator >> (istream& is, Fraction& fraction)
-{  char ch;
-  is >> fraction.numerator;   //get the numerator  is >> ch;                   //read and discard the '/'  is >> fraction.denominator; //get the denominator  return is;
+//**********************************************
+// Overloaded >> operator
+//**********************************************
+istream& operator >> (istream& is, Fraction& fraction)
+{
+  char ch;
+
+  is >> fraction.numerator;   //get the numerator
+  is >> ch;                   //read and discard the '/'
+  is >> fraction.denominator; //get the denominator
+  return is;
 }
